33c268cae709074b185955006303807002202b9d
Ivan Larionov <xlebnica.booroop@gmail.com>
2019-08-30 14:16:53 +0700

    Add files via upload

---
+++ SampleIntegration.py
@@ -0,0 +1,20 @@
+import OVIntegration
+import json
+import time
+
+with open('PasswordFile.json', "rb") as PFile:
+    passwordData = json.loads(PFile.read().decode('utf-8'))
+
+user = passwordData["UserName"]
+password = passwordData["Password"]
+site = passwordData["URL"]
+
+with open('ihub_process_id', "rb") as PFile:
+    processId = PFile.read().decode('utf-8')
+
+i = 0
+while i < 3:
+    time.sleep(5)
+    integrationOV = OVIntegration.OVIntegration(processId=processId, url=site, userName=user, password=password)
+    i = i + 1
+    print(i, flush = True)
